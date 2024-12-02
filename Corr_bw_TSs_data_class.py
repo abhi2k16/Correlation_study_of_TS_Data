2dc004caa6c7131181cff36041795ee5b6b15c76
Abhijeet Kumar <31257269+abhi2k16@users.noreply.github.com>
2024-12-02 13:46:17 +0530

    Update Corr_bw_TSs_data_class.py

--- Corr_bw_TSs_data_class.py
+++ Corr_bw_TSs_data_class.py
@@ -12,7 +12,7 @@ parameters = ['N2_RATE','APRS_RAW','IPRS_RAW','WOB_DH','CT_WGT',
               'TEMP_DNI_RAW', 'ATEMP_RAW', 'PTEMP_RAW', 'DAGR_Temp','DEPT','INCL_RT_RAW',
               'AZIM_RT_RAW'] 
 #data = pd.read_csv("Pandas_dataframe_O_1011724_56-7.csv")
-data = pd.read_csv("C:/Users/AKumar340/OneDrive - SLB/2024/CTD_EventDetection/Data/O.1011724.56-5.csv")
+data = pd.read_csv("*.csv")
 N2data = data[['TIME','N2_RATE']]
 APRS_RAWdata = data[['TIME','APRS_RAW']]
 #%% Plot the observed data 
